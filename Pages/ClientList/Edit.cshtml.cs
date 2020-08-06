0563fb67be09a034ec87dcada045a206df46106e
Patryk Dublinowski <p.dublinowski@sputnik.pl>
2020-08-06 13:03:14 +0200

    Add edit functionality

---
+++ Pages/ClientList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using CarServiceClients.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarServiceClients.Pages.ClientList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Client Client { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Client = await _db.Client.FindAsync(id); 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var ClientFromDb    = await _db.Client.FindAsync(Client.Id);
+                ClientFromDb.Name   = Client.Name;
+                ClientFromDb.Car    = Client.Car;
+                ClientFromDb.Status = Client.Status;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
